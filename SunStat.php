2464a0a701be57948b552cee62be0014577897f5
Vitaliy Zhukov <DragonZX@yandex.ru>
2016-10-17 12:56:28 +0300

    Varibles in functions

--- SunStat.php
+++ SunStat.php
@@ -45,93 +45,112 @@ class plgSystemSunStat extends JPlugin
         {
             return;
         }
-        function MailRu ($mr_id){}
 
-        function YandexMetrika ($ym_id){}
+        function MailRu (){
+			// Initialise variables
+        	$mr_enabled 			= $this->params->get( 'mr_enabled', '' );
+			$mr_noIndexWrapper 		= $this->params->get( 'mr_noindexWrapper', '1' );
+			$mr_id 					= $this->params->get( 'mr_id', '' );
 
-        function GoogleAnalytics ($ga_id) {}
+			$script		= '<!-- Rating Mail.ru counter --><script type="text/javascript">//<![CDATA[var _tmr = _tmr || [];_tmr.push({id: '.$mr_id.', type: "pageView", start: (new Date()).getTime()});(function (d, w) {   var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true;   ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";   var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};   if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }})(document, window);//]]></script><noscript><div style="position:absolute;left:-10000px;"><img src="//top-fwz1.mail.ru/counter?id='.$mr_id.';js=na" style="border:0;" height="1" width="1" alt="Rating Mail.ru" /></noscript><!-- /Rating Mail.ru counter -->';
+			if($mr_noIndexWrapper) $script = '<!--noindex-->' . $script . '<!--/noindex-->';
 
-        function PiwikCounter (){}
+			return $script;
+		}
 
-        function LiveInternet () {}
+        function YandexMetrika (){
+			// Initialise variables
+        	$ym_enabled 			= $this->params->get( 'ym_enabled', '' );
+			$ym_noIndexWrapper 		= $this->params->get( 'ym_noindexWrapper', '1' );
+			$ym_id 					= $this->params->get( 'ym_id', '' );
+			$ym_yaParams			= $this->params->get( 'ym_yaParams', '' );
+			$ym_trackHash			= $this->params->get( 'ym_trackHash', '' );
+			$ym_webvisor 			= $this->params->get( 'ym_webvisor', '' );
+			$ym_clickMap 			= $this->params->get( 'ym_clickMap', '' );
+			$ym_linksOut 			= $this->params->get( 'ym_linksOut', '' );
+			$ym_accurateTrackBounce = $this->params->get( 'ym_accurateTrackBounce', '' );
+			$ym_noIndex 			= $this->params->get( 'ym_noIndex', '' );
 
-        function OpenStat (){}
+			if ($ym_yaParams !=''){$ym_ef_yaParams='<script type="text/javascript">var yaParams = {'.$ym_yaParams.'};</script>'; $ym_ef_yaParams2='params:window.yaParams||{ }';} else
+			{$ym_ef_yaParams=''; $ym_ef_yaParams2='';};
+			if ($ym_webvisor)		$ym_ef_webvisor	="webvisor:true, "; 										 				else $ym_ef_webvisor='';
+			if ($ym_clickMap)		$ym_ef_clickmap	="clickmap:true, "; 										 				else $ym_ef_clickmap='';
+			if ($ym_linksOut)		$ym_ef_linksout	="trackLinks:true, "; 										 			else $ym_ef_linksout='';
+			if ($ym_accurateTrackBounce) $ym_ef_atb	="accurateTrackBounce:true, "; 											else $ym_ef_atb='';
+			if ($ym_trackHash)	$ym_ef_trackhash="accurateTrackBounce:true, "; 											else $ym_ef_trackhash='';
+			if ($ym_noIndex)		{$ym_ef_noindex	='ut:"noindex", '; $ym_ef_noindex2='?ut=noindex';} 									else {$ym_ef_noindex=''; $ym_ef_noindex2='';};
 
-        function HotLog (){}
+		}
 
-        function RamblerTop (){}
-        
-		// Initialise variables
-		//mailru
-		$mr_enabled 			= $this->params->get( 'mr_enabled', '' );
-		$mr_noIndexWrapper 		= $this->params->get( 'mr_noindexWrapper', '1' );
-		$mr_id 					= $this->params->get( 'mr_id', '' );
-		//yandex metrika
-		$ym_enabled 			= $this->params->get( 'ym_enabled', '' );
-		$ym_noIndexWrapper 		= $this->params->get( 'ym_noindexWrapper', '1' );
-		$ym_id 					= $this->params->get( 'ym_id', '' );
-		$ym_yaParams			= $this->params->get( 'ym_yaParams', '' );
-		$ym_trackHash			= $this->params->get( 'ym_trackHash', '' );
-        $ym_webvisor 			= $this->params->get( 'ym_webvisor', '' );
-		$ym_clickMap 			= $this->params->get( 'ym_clickMap', '' );
-        $ym_linksOut 			= $this->params->get( 'ym_linksOut', '' );
-        $ym_accurateTrackBounce = $this->params->get( 'ym_accurateTrackBounce', '' );
-        $ym_noIndex 			= $this->params->get( 'ym_noIndex', '' );
-		//google analytics
-		$ga_enabled 			= $this->params->get( 'ga_enabled', '' );
-		$ga_legacy	 			= $this->params->get( 'ga_legacy', '' );
-		$ga_noIndexWrapper 		= $this->params->get( 'ga_noindexWrapper', '1' );
-		$ga_id 					= $this->params->get( 'ga_id', '' );
-		$ga_domain 				= $this->params->get( 'ga_domain', '' );
-		$ga_uid 				= $this->params->get( 'ga_uid', '' );
-		$ga_demographic			= $this->params->get( 'ga_demographic', '' );
-		$ga_extattrib			= $this->params->get( 'ga_extattrib', '' );
-		//piwik
-		$pwk_enabled 			= $this->params->get( 'pwk_enabled', '' );
-		$pwk_noIndexWrapper 	= $this->params->get( 'pwk_noindexWrapper', '1' );
-		$pwk_addDomain			= $this->params->get( 'pwk_addDomain', '' );
-		$pwk_subdomains 		= $this->params->get( 'pwk_subdomains', '' );
-		$pwk_linksOut 			= $this->params->get( 'pwk_linksOut', '' );
-		$pwk_address 			= $this->params->get( 'pwk_address', '' );
-		$pwk_domain 			= $this->params->get( 'pwk_domain', '' );
-		//liveinternet
-		$li_enabled 			= $this->params->get( 'li_enabled', '' );
-		$li_noIndexWrapper 		= $this->params->get( 'li_noindexWrapper', '1' );
-		//OpenStat
-		$os_enabled 			= $this->params->get( 'os_enabled', '' );
-		$os_identified			= $this->params->get( 'os_identified', '' );
-		$os_id 					= $this->params->get( 'os_id', '' );
-		$os_noIndexWrapper 		= $this->params->get( 'os_noindexWrapper', '1' );
-		//hotlog
-		$hl_enabled 			= $this->params->get( 'hl_enabled', '' );
-		$hl_noIndexWrapper 		= $this->params->get( 'hl_noindexWrapper', '1' );
-		$hl_id 					= $this->params->get( 'hl_id', '' );
-		//rambler
-		$rr_enabled 			= $this->params->get( 'rr_enabled', '' );
-		$rr_noIndexWrapper 		= $this->params->get( 'rr_noindexWrapper', '1' );
-		$rr_id 					= $this->params->get( 'rr_id', '' );
+        function GoogleAnalytics () {
+			// Initialise variables
+			$ga_enabled 			= $this->params->get( 'ga_enabled', '' );
+			$ga_legacy	 			= $this->params->get( 'ga_legacy', '' );
+			$ga_noIndexWrapper 		= $this->params->get( 'ga_noindexWrapper', '1' );
+			$ga_id 					= $this->params->get( 'ga_id', '' );
+			$ga_domain 				= $this->params->get( 'ga_domain', '' );
+			$ga_uid 				= $this->params->get( 'ga_uid', '' );
+			$ga_demographic			= $this->params->get( 'ga_demographic', '' );
+			$ga_extattrib			= $this->params->get( 'ga_extattrib', '' );
+
+			//Google Analytics
+			if ($ga_demographic)$ga_ef_demographic	="ga('require', 'displayfeatures');"; 										 else $ga_ef_demographic='';
+			if ($ga_extattrib) 	$ga_ef_extattrib	="ga('require', 'linkid', 'linkid.js');";									 else $ga_ef_extattrib='';
+			if ($ga_uid) 		$ga_ef_uid			='ga(‘set’, ‘&uid’, {{USER_ID}});'; 										 else $ga_ef_uid='';
+			//Google legacy
+			if ($ga_demographic)$gal_ef_demographic	="ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';"; 										 else $gal_ef_demographic="ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';";
+			if ($ga_extattrib) 	$gal_ef_extattrib	="var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js'; _gaq.push(['_require', 'inpage_linkid', pluginUrl]);";						else $gal_ef_extattrib='';
+
+
+		}
+
+        function PiwikCounter (){
+			// Initialise variables
+			$pwk_enabled 			= $this->params->get( 'pwk_enabled', '' );
+			$pwk_noIndexWrapper 	= $this->params->get( 'pwk_noindexWrapper', '1' );
+			$pwk_addDomain			= $this->params->get( 'pwk_addDomain', '' );
+			$pwk_subdomains 		= $this->params->get( 'pwk_subdomains', '' );
+			$pwk_linksOut 			= $this->params->get( 'pwk_linksOut', '' );
+			$pwk_address 			= $this->params->get( 'pwk_address', '' );
+			$pwk_domain 			= $this->params->get( 'pwk_domain', '' );
+		}
+
+        function LiveInternet () {
+			// Initialise variables
+			$li_enabled 			= $this->params->get( 'li_enabled', '' );
+			$li_noIndexWrapper 		= $this->params->get( 'li_noindexWrapper', '1' );
+		}
+
+        function OpenStat (){
+			// Initialise variables
+			$os_enabled 			= $this->params->get( 'os_enabled', '' );
+			$os_identified			= $this->params->get( 'os_identified', '' );
+			$os_id 					= $this->params->get( 'os_id', '' );
+			$os_noIndexWrapper 		= $this->params->get( 'os_noindexWrapper', '1' );
+		}
+
+        function HotLog (){
+			// Initialise variables
+			$hl_enabled 			= $this->params->get( 'hl_enabled', '' );
+			$hl_noIndexWrapper 		= $this->params->get( 'hl_noindexWrapper', '1' );
+			$hl_id 					= $this->params->get( 'hl_id', '' );
+		}
+
+        function RamblerTop (){
+			// Initialise variables
+        	$rr_enabled 			= $this->params->get( 'rr_enabled', '' );
+			$rr_noIndexWrapper 		= $this->params->get( 'rr_noindexWrapper', '1' );
+			$rr_id 					= $this->params->get( 'rr_id', '' );
+        }
+
+
 		
 		//getting body code and storing as buffer
 		$buffer = JResponse::getBody();
 		
 		//Extended functions
 		//Yandex Metrika
-		if ($ym_yaParams !=''){$ym_ef_yaParams='<script type="text/javascript">var yaParams = {'.$ym_yaParams.'};</script>'; $ym_ef_yaParams2='params:window.yaParams||{ }';} else 
-		{$ym_ef_yaParams=''; $ym_ef_yaParams2='';};
-		if ($ym_webvisor)		$ym_ef_webvisor	="webvisor:true, "; 										 				else $ym_ef_webvisor='';
-		if ($ym_clickMap)		$ym_ef_clickmap	="clickmap:true, "; 										 				else $ym_ef_clickmap='';
-		if ($ym_linksOut)		$ym_ef_linksout	="trackLinks:true, "; 										 			else $ym_ef_linksout='';
-		if ($ym_accurateTrackBounce) $ym_ef_atb	="accurateTrackBounce:true, "; 											else $ym_ef_atb='';
-		if ($ym_trackHash)	$ym_ef_trackhash="accurateTrackBounce:true, "; 											else $ym_ef_trackhash='';
-		if ($ym_noIndex)		{$ym_ef_noindex	='ut:"noindex", '; $ym_ef_noindex2='?ut=noindex';} 									else {$ym_ef_noindex=''; $ym_ef_noindex2='';};
-		//Google Analytics
-		if ($ga_demographic)$ga_ef_demographic	="ga('require', 'displayfeatures');"; 										 else $ga_ef_demographic='';
-		if ($ga_extattrib) 	$ga_ef_extattrib	="ga('require', 'linkid', 'linkid.js');";									 else $ga_ef_extattrib='';
-		if ($ga_uid) 		$ga_ef_uid			='ga(‘set’, ‘&uid’, {{USER_ID}});'; 										 else $ga_ef_uid='';
-		//Google legacy
-		if ($ga_demographic)$gal_ef_demographic	="ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';"; 										 else $gal_ef_demographic="ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';";
-		if ($ga_extattrib) 	$gal_ef_extattrib	="var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js'; _gaq.push(['_require', 'inpage_linkid', pluginUrl]);";						else $gal_ef_extattrib='';
-		//Piwik
+//Piwik
 		if ($pwk_linksOut) 	$pwk_ef_linksOut	='_paq.push(["setDomains", ["*.'.$pwk_domain.'"]]); '; 						 else $pwk_ef_linksOut='';
 		if ($pwk_subdomains)$pwk_ef_subdomains	='_paq.push(["setCookieDomain", "*.'.$pwk_domain.'"]); '; 					 else $pwk_ef_subdomains='';
 		if ($pwk_addDomain)	$pwk_ef_addDomain	='_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);'; else $pwk_ef_addDomain='';
@@ -139,8 +158,7 @@ class plgSystemSunStat extends JPlugin
 		if (!$os_identified) $os_id = 1;
 
 		//Init counters
-        $mr		= '<!-- Rating Mail.ru counter --><script type="text/javascript">//<![CDATA[var _tmr = _tmr || [];_tmr.push({id: '.$mr_id.', type: "pageView", start: (new Date()).getTime()});(function (d, w) {   var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true;   ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";   var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};   if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }})(document, window);//]]></script><noscript><div style="position:absolute;left:-10000px;"><img src="//top-fwz1.mail.ru/counter?id='.$mr_id.';js=na" style="border:0;" height="1" width="1" alt="Rating Mail.ru" /></noscript><!-- /Rating Mail.ru counter -->';
-		if($mr_noIndexWrapper) $mr = '<!--noindex-->' . $mr . '<!--/noindex-->';
+
 		$ym		= '<!-- Yandex.Metrika counter -->' . $ym_ef_yaParams . '<script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter' . $ym_id . ' = new Ya.Metrika({id:' . $ym_id . ', ' .$ym_ef_webvisor.$ym_ef_clickmap.$ym_ef_linksout.$ym_ef_atb.$ym_ef_trackhash.$ym_ef_noindex.$ym_ef_yaParams2.'}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/' . $ym_id .$ym_ef_noindex2. '" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->';
 		if($ym_noIndexWrapper) $ym = '<!--noindex-->' . $ym . '<!--/noindex-->';
 		$ga		= "<!-- Universal Analytics counter --><script type='text/javascript'>  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-".$ga_id."', '".$ga_domain."'); ".$ga_ef_demographic.$ga_ef_extattrib." ga('send', 'pageview'); ".$ga_ef_uid." </script><!-- /Universal Analytics counter -->";	
